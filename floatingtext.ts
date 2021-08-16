ec67427627f6eb78ba06fc62b08deb88722b5c8c
tcfplayz <58693033+ckclol@users.noreply.github.com>
2021-08-16 21:54:51 +0800

    i forgot

--- floatingtext.ts
+++ floatingtext.ts
@@ -10,4 +10,5 @@ export function floatingTextTest(text: string, x: number, y: number, z: number) 
         const adi = ActorDefinitionIdentifier.create(ActorType.ArmorStand);
         const entity = spawnEntity(player.getRegion(), Vec3.create(x, y, z), adi, generateUniqueID(serverInstance.minecraft.getLevel()), new Actor());
         entity.addEffect(MobEffectInstance.create(MobEffectIds.Invisibility, 1, 1, false, true, false));
+        entity.setName(text);
 }
