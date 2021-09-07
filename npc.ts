b58b1632281ed296586b6e624b08b5d53ce2fc11
tcfplayz <58693033+ckclol@users.noreply.github.com>
2021-09-07 18:12:07 +0800

    Update and rename fakeplayer.ts to npc.ts

--- npc.ts
+++ npc.ts
@@ -5,10 +5,10 @@ import { Vec3 } from 'bdsx/bds/blockpos';
 import { spawnEntity, generateUniqueID } from './mobspawning';
 // WIP
 
-class SlapperPlayerBase extends ServerPlayer {
+class SlapperNPCBase extends Actor {
         create() {
                 const player = serverInstance.minecraft.getLevel().players.get(0);
-                const adi = ActorDefinitionIdentifier.create(ActorType.Player);
+                const adi = ActorDefinitionIdentifier.create(ActorType.NPC);
                 const entity = spawnEntity(player.getRegion(), Vec3.create(200, 100, 200), adi, generateUniqueID(serverInstance.minecraft.getLevel()), new Actor());
         }
 }
