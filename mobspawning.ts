c37e089f6b47e52b5b54411c75d8ea84e7c923b3
tcfplayz <58693033+ckclol@users.noreply.github.com>
2021-10-13 20:07:18 +0800

    replace with actor.spawn i think idkkk

--- mobspawning.ts
+++ mobspawning.ts
@@ -6,14 +6,8 @@ import { ProcHacker } from "bdsx/prochacker";
 import { Vec3 } from 'bdsx/bds/blockpos';
 import { Level } from 'bdsx/bds/level';
 
-const hacker = ProcHacker.load("../pdbcache.ini", ["CommandUtils::spawnEntityAt", "Level::getNewUniqueID"], UNDNAME_NAME_ONLY);
+const hacker = ProcHacker.load("../pdbcache.ini", ["Level::getNewUniqueID"], UNDNAME_NAME_ONLY);
 pdb.close();
-
-var spawnEntity2 = hacker.hooking("CommandUtils::spawnEntityAt", Actor, null, BlockSource, Vec3, ActorDefinitionIdentifier, ActorUniqueID, Actor)(spawnEntity);
-
-export function spawnEntity(bs: BlockSource, vec: Vec3, adi: ActorDefinitionIdentifier, auid: ActorUniqueID, a: Actor) {
-        return spawnEntity2(bs, vec, adi, auid, a);
-}
 
 var newauid = hacker.hooking("Level::getNewUniqueID", ActorUniqueID, null, Level)(generateUniqueID);
 
